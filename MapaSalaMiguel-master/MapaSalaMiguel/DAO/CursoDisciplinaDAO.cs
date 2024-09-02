2357112e7f376b784347039d95988d20c840171d
Aluno <Aluno@LS05M10>
2024-09-02 13:46:10 -0300

    fiz nada

---
+++ MapaSalaMiguel-master/MapaSalaMiguel/DAO/CursoDisciplinaDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaSalaMiguel.DAO
+{
+    public void Inserir(CursoDisciplinaEntidade cursoDisciplina)
+    {
+        Conexao.Open();
+        string query = "Insert into CursoDisciplina" + (Curso_Id, Apelido) Values (@nome,@apelido)";
+        SqlCommand comando = new SqlCommand(query, Conexao);
+        SqlParameter parametro1 = new SqlParameter("@Curso_Id", Objeto.CursoId);
+        SqlParameter parametro2 = new SqlParameter("@Disciplina_Id", Objeto.Disciplina);
+        SqlParameter parametro2 = new SqlParameter("@Disciplina_Id", Objeto.Periodo);
+        comando.Parameters.Add(parametro1);
+        comando.Parameters.Add(parametro2);
+        comando.ExecuteNonQuery();
+        Conexao.Close();
+    }
+    class CursoDisciplinaDAO
+    {
+    }
+}
---
+++ MapaSalaMiguel-master/model/entidades/CursoDisciplina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model.entidades
+{
+    class CursoDisciplina
+    {
+        public long id { get; set; }
+        public long DisciplinaId { get; set; }
+        public long CursoId { get; set; }
+        public string Periodo { get; set; }
+        public string NomeDisciplina { get; set; }
+        public String NomeCurso { get; set; }
+    }
+}
